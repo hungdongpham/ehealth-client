25d33f918b3846a0d435a749bbd3c74e85ea250e
Tri dep <nguyentri0401@gmail.com>
2018-04-11 23:45:26 +0700

    asdasdasd

--- app.js
+++ app.js
@@ -52,8 +52,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(80,function(){
-  console.log("Live at Port 3000");
+app.listen(3000,function(){
+  console.log("Live at Port 3000x");
 });
 
 
